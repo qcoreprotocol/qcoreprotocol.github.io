c0b5f74016dd55290c93d8adc7b89a30e5edc151
devs <doc1@gmail.com>
2020-10-03 21:10:23 +1000

    main

---
+++ precache-manifest.8c5a2f7ef8993d3707a8e75a3c5bf7ed.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2e9e9eab48e681cfc63f6384148a556",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bf53494a2f79ac3cf7e",
+    "url": "/static/css/main.3adc54ec.chunk.css"
+  },
+  {
+    "revision": "9e2edb8ae8ae77b21224",
+    "url": "/static/js/2.dc6b09a9.chunk.js"
+  },
+  {
+    "revision": "402769e0fde7682650babf6b5720df16",
+    "url": "/static/js/2.dc6b09a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bf53494a2f79ac3cf7e",
+    "url": "/static/js/main.69d9083a.chunk.js"
+  },
+  {
+    "revision": "7a6956e196ccd2e4ccc8856708b02847",
+    "url": "/static/js/main.69d9083a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aa3d7f9ac5e47205f7b",
+    "url": "/static/js/runtime-main.d7651f20.js"
+  },
+  {
+    "revision": "f822e81ea670e20928179e21eb6f87bd",
+    "url": "/static/media/airdrop.f822e81e.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "4ffd4fe7945af123788bf5888296c696",
+    "url": "/static/media/github.4ffd4fe7.svg"
+  },
+  {
+    "revision": "87be59a1f7061fc6021876aad6fee028",
+    "url": "/static/media/google.87be59a1.svg"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/static/media/ill-2.594b1ee1.svg"
+  },
+  {
+    "revision": "1e7c70dc7d471d845aeed9e9bead0fe5",
+    "url": "/static/media/img-1-1200x1000.1e7c70dc.jpg"
+  },
+  {
+    "revision": "560c9c86401007b7448464e73eb0852a",
+    "url": "/static/media/meme1.560c9c86.png"
+  },
+  {
+    "revision": "cfa0fb207c3183f50b79c6d00b548266",
+    "url": "/static/media/nft2.cfa0fb20.png"
+  },
+  {
+    "revision": "371d6faf4809b7d33ae9f082ab56d29c",
+    "url": "/static/media/nft3.371d6faf.png"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "ab840b2e208a8a861ec66454ab7a2f5e",
+    "url": "/static/media/promo-1.ab840b2e.png"
+  },
+  {
+    "revision": "53033970a416368da35794389680266f",
+    "url": "/static/media/team-1-800x800.53033970.jpg"
+  },
+  {
+    "revision": "dcfcf3b77210fe85b0abc8260e6fa70e",
+    "url": "/static/media/team-2-800x800.dcfcf3b7.jpg"
+  },
+  {
+    "revision": "497bb3590e24c9f8b645864bfffc39b8",
+    "url": "/static/media/team-3-800x800.497bb359.jpg"
+  },
+  {
+    "revision": "230071328b705f8686cabd26a85ed6a5",
+    "url": "/static/media/team-4-800x800.23007132.jpg"
+  }
+]);
